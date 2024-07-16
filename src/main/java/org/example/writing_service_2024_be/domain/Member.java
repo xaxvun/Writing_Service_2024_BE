c64386bac38f461430a56865dd7ab01f668d1e9b
dahyun <fbekgus413@naver.com>
2024-07-17 01:12:28 +0900

    Member 수정중

--- src/main/java/org/example/writing_service_2024_be/domain/Member.java
+++ src/main/java/org/example/writing_service_2024_be/domain/Member.java
@@ -1,4 +1,11 @@
 package org.example.writing_service_2024_be.domain;
 
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Member {
 }
